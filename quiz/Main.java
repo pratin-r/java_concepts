a66ba0f846258b34b009f77752b6f0750e14a7b1
Prathin <pratinrp@gmail.com>
2025-11-19 22:09:36 +0530

    added subpackage quiz

---
+++ quiz/Main.java
@@ -0,0 +1,43 @@
+/*
+ * to do later
+ * What if multiple trainers want to access the switch case
+ */
+package quiz;
+
+import java.util.Scanner;
+
+import quiz.candidates.CandidatesService;
+import quiz.trainer.QuestionService;
+
+@SuppressWarnings("unused")
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of Questions you want to store: ");
+        int numberOfQuestions = sc.nextInt();
+        QuestionService trainer1 = new QuestionService(numberOfQuestions, sc);
+        // System.out.println(
+        //         "Press 1 for displaying questions\nPress 2 for displaying answers\nPress 3 to close the service");
+        // int choice = sc.nextInt();
+        // switch (choice) {
+        //     case 1:
+        //         System.out.print("Enter the question number: ");
+        //         int questionNo = sc.nextInt();
+        //         trainer1.displayQuestion(questionNo);
+        //         break;
+        //     case 2:
+        //         System.out.print("Enter the question number: ");
+        //         int ansNo = sc.nextInt();
+        //         trainer1.displayAnswer(ansNo);
+        //         break;
+        //     case 3:
+        //         System.out.println("Thank you");
+        //         break;
+        //     default:
+        //         System.out.println("Enter a proper question number !!!");
+        //         break;
+        // }
+        CandidatesService obj=new CandidatesService(trainer1);
+        obj.playTest(trainer1, sc);
+    }
+}
---
+++ quiz/candidates/CandidatesService.java
@@ -0,0 +1,39 @@
+package quiz.candidates;
+
+import java.util.Scanner;
+
+import quiz.trainer.QuestionService;
+
+public class CandidatesService {
+    public int score;
+    public int[] answers;
+
+    public CandidatesService(QuestionService trainerNo) {
+        answers = new int[trainerNo.questions.length];
+    }
+
+    public void playTest(QuestionService trainerNo, Scanner sc) {
+        if (trainerNo.questions.length == 0 || trainerNo.questions[0] == null) {
+            System.out.println("Questions are not available");
+            return;
+        }
+        for (int i = 0; i < trainerNo.questions.length; i++) {
+            trainerNo.displayQuestion(i);
+            System.out.println("\n");
+            trainerNo.displayOptions(i);
+            System.out.print("Enter your option: ");
+            answers[i] = sc.nextInt();
+        }
+        sc.close();
+        displayScore(trainerNo);
+    }
+
+    private void displayScore(QuestionService trainerNo) {
+        for (int i = 0; i < trainerNo.questions.length; i++) {
+            if (trainerNo.questions[i].getAnswer() == answers[i]) {
+                score++;
+            }
+        }
+        System.out.println("Your Score is: " + score+" out of "+trainerNo.questions.length);
+    }
+}

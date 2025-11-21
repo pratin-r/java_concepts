1e9e348215fcb9c7c259f313b2fba15f6982e2d3
Prathin <pratinrp@gmail.com>
2025-11-21 20:50:22 +0530

    added small change

--- advancejava/exceptionhandling/DemoException.java
+++ advancejava/exceptionhandling/DemoException.java
@@ -8,14 +8,10 @@ class InvalidAgeException extends Exception {
 
 public class DemoException {
     public static void checkAge(int age) throws InvalidAgeException {
-        try {
-            if (age < 18) {
-                throw new InvalidAgeException("Exception caught: Age must be 18 or older");
-            }
-            System.out.println("Access granted");
-        } catch (InvalidAgeException e) {
-            System.out.println(e.getMessage());
+        if (age < 18) {
+            throw new InvalidAgeException("Exception caught: Age must be 18 or older");
         }
+        System.out.println("Access granted");
     }
 
     public static void main(String[] args) {
